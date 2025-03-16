aa1c6f31825655c4ad8fab5067c75257ba7158ed
thara-1997 <thathsaraninayanathara316@gmail.com>
2025-03-16 22:24:16 +0530

    firt initial commit

--- src/test/java/com/pragmatic/SauceDemo/runner/TestRunner.java
+++ src/test/java/com/pragmatic/SauceDemo/runner/TestRunner.java
@@ -4,7 +4,11 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = {"src/test/resources/Features/product.feature"},
+        features = {"src/test/resources/Features/product.feature",
+                    "src/test/resources/Features/cart.feature",
+                    "src/test/resources/Features/checkout.feature",
+                    "src/test/resources/Features/login.feature",
+                    "src/test/resources/Features/navigationAndLogout.feature"},
         glue = {"com.pragmatic.SauceDemo.steps"},
         //name = {"Login with invalid credentials"},
         //tags = "@smoke or @regression",
